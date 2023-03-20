c0d49650468efd2a46e86099735e56918c8aacf8
BlacksilverConsulting <104861306+BlacksilverConsulting@users.noreply.github.com>
2023-03-19 20:19:41 -0400

    Undo OS8/9 merge

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ set -o errexit
 # See LICENSE file for license information
 
 # Invocation (run as root, soory):
-#  cd && curl -LJO https://blacksilverconsulting.github.io/OS8/start.sh && bash start.sh
+#  cd && curl -O https://blacksilverconsulting.github.io/OS8/start.sh && bash start.sh
 
 # Description:
 #  This script is designed to start the process of setting up a new install of
@@ -17,22 +17,26 @@ set -o errexit
 #  - Yes this is full of security holes. PRs welcome! 
 #  - This script assumes that it is running as root, and running in /root
 
-# Enable EPEL
+echo 'Enable EPEL'
 dnf -y install epel-release epel-next-release
 
-# Install Ansible and dependencies, can't be combined with EPEL
+echo 'Install Ansible and dependencies'
 dnf -y install python3 python3-rpm python3-pycurl sshpass ansible-core \
 ansible-collection-ansible-posix ansible-collection-community-general \
 ansible-collection-redhat-rhel_mgmt 
 
-# Download the playbook, leaving a copy in the current directory
+echo 'Download the playbooks'
 curl -LJO https://github.com/BlacksilverConsulting/OS8/raw/main/base.yaml
+curl -LJO https://github.com/BlacksilverConsulting/OS8/raw/main/pg14.yaml
+curl -LJO https://github.com/BlacksilverConsulting/OS8/raw/main/dm.yaml
+
+echo 'Run the base playbook'
 ansible-playbook ./base.yaml
 
-echo Initial configuration complete.
+echo 'Initial configuration complete.'
 
-echo To install PostgreSQL 14 Server and Client:
-echo 'curl -LJO https://github.com/BlacksilverConsulting/OS8/raw/main/pg14.yaml && ansible-playbook ./pg14.yaml'
+echo 'To install PostgreSQL 14 Server and Client:'
+echo 'ansible-playbook ./pg14.yaml'
 
-echo To install other components useful for document management:
-echo 'curl -LJO https://github.com/BlacksilverConsulting/OS8/raw/main/dm.yaml && ansible-playbook ./dm.yaml'
+echo 'To install other components useful for document management:'
+echo 'ansible-playbook ./dm.yaml'
